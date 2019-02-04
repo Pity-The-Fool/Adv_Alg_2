2d3c6f5e37278e6f359fd1cbafa7856ebc1d75a9
Dominic Farolino <domfarolino@gmail.com>
2019-02-04 12:41:14 -0500

    This commit:
     - Utilizes the hashlib to create an integer value out of the message
     - Adds some basic timing output

--- Homework_1/common/cli_parser.py
+++ Homework_1/common/cli_parser.py
@@ -2,15 +2,15 @@ import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-m", "--message",
                         help="Type a message to sign and verify",
                         dest="message",
                         type=str,
                         default="Cryptographic Message"
                         )
+    return parser
 
-    def parse_args(args=None):
-        if args is None:
-            return get_parser().parse_args()
-        return get_parser().parse_args(args)
+def parse_args(args=None):
+    if args is None:
+        return get_parser().parse_args()
+    return get_parser().parse_args(args)

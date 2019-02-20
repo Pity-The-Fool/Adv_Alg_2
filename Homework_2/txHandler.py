c467cf9c4b5fa2c3613e01f0f7005b4ff4e80b9e
ndaughety <ndaughety13@gmail.com>
2019-02-20 10:23:43 -0500

    still digging into code

--- Homework_2/txHandler.py
+++ Homework_2/txHandler.py
@@ -1,16 +1,49 @@
 # You will be responsible for creating a module that contains an API called txHandler that validates transactions using UTXOpool and updates the pool based on processing a set of non-conflicting tranactions contained in the pool.
 
-import UTXOPool
+from UTXO import UTXOPool
+from transaction import transaction
+from digital_signature import digital_signature
 
-def isValidTx(Transaction tx):
+NUM_OUPUTS = 2
+NUM_INPUTS = 2
+
+def isValidTx(Transaction tx, args=None):
+    result = false
+    txInput = transaction.Transaction.Input()
+    txOutput = transaction.Transaction.Output()
+
     # Returns true if
     # (1) all outputs claimed by tx are in the current UTXO pool,
+    for i in range(2):
+        if txOutput.getOutput(NUM_OUTPUTS) == UTXOPool.getTxOutput(NUM_OUTPUTS):
+            result = true
+        else:
+            return false
+
     # (2) the signatures on each input of tx are valid,
+    ds = digital_signature.DigitalSignature(args);
+    for i in range(NUM_INPUTS):
+        ds.sign(txInput.getRawDataToSign(i))
+        if ds.verify():
+            result = true
+        else:
+            return false
+
     # (3) no UTXO is claimed multiple times by tx,
+
+
     # (4) all of tx’s output values are non-negative, and
+     for i in range(NUM_OUTPUTS):
+        if txOutput.getOutput(i) >= 0:
+            result = true
+        else:
+            return false
+
     # (5) the sum of tx’s input values is greater than or
     #     equal to the sum of its output values; and false otherwise.
-    return false;
+
+
+    return result
 
 
 def handleTxs(possibleTxs): # Transaction[] --> Transaction[]

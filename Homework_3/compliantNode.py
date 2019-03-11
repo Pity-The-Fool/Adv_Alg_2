7aa8db33d77703d6a9064cfc58d3808f165f8461
ndaughety <ndaughety13@gmail.com>
2019-03-11 11:15:53 -0400

    added CompliantNode class -- not finished

---
+++ Homework_3/compliantNode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class CompliantNode:
+    # Node Interface API
+
+    def​ ​setFollowees​ (followees):
+    def​ ​setPendingCatx​ (pendingCatxs):
+    def​ ​sendToFollowers​(candidates):
+    def​ ​receiveFromFollowees​ (candidates):
+
+    #Consensus: After final ​round​, ​for​ each node, sendToFollowers() should ​return​ the Catxs upon which consensus has been reached.

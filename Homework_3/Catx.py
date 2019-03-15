f720592ec53cb8c18d0aec99fd7442b5ff955ae6
ndaughety <ndaughety13@gmail.com>
2019-03-14 22:03:14 -0400

    changed encoding

--- Homework_3/Catx.py
+++ Homework_3/Catx.py
@@ -2,10 +2,10 @@
 
 ## Catx Interface API
 
-scripts ​=​ { ​P2PKH​ :​"OP_DUP OP_HASH160 <Public-Key-Hash> OP_EQUALVERIFY OP_CHECKSIG"​,
-            P2PT​: ​"Pay to Post UC Tribute <shortened -url>"​,
-            P2UCS​: ​"Pay to UC Scholar <Public-Key-Hash>"​,
-            P2HS​ : ​"Pay to Hashed Script <Hashed-Address>"​,
-            OTHER​: ​"Another Script"​ }
+scripts ​=​ { P2PKH: "OP_DUP OP_HASH160 <Public-Key-Hash> OP_EQUALVERIFY OP_CHECKSIG",
+            P2PT: "Pay to Post UC Tribute <shortened -url>",
+            P2UCS: "Pay to UC Scholar <Public-Key-Hash>",
+            P2HS: "Pay to Hashed Script <Hashed-Address>",
+            OTHER: "Another Script" }
 
-def​ ​encodeCatx​(script, ​*​args, ​**​kwargs): ​return​ code
+def encodeCatx(script, *args, **kwargs): return code

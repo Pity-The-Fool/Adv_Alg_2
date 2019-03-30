8f5424efdedec4995b27ba0540838c29878034b6
ndaughety <ndaughety13@gmail.com>
2019-03-30 13:01:55 -0400

    finished -- cleaned up indentations

--- Homework_3/Candidate.py
+++ Homework_3/Candidate.py
@@ -3,4 +3,4 @@
 class Candidate():
     def __init__(self, tx,sender):
         self.tx = tx
-	self.sender = sender
+        self.sender = sender
--- Homework_3/simulation.py
+++ Homework_3/simulation.py
@@ -11,11 +11,11 @@ from Transaction import Transaction
 
 
 def main(args):
-##      // There are four required command line arguments: p_graph (.1, .2, .3),
-##      // p_malicious (.15, .30, .45), p_txDistribution (.01, .05, .10),
-##      // and numRounds (10, 20). You should try to test your CompliantNode
-##      // code for all 3x3x3x2 = 54 combinations.
-##
+    ##      // There are four required command line arguments: p_graph (.1, .2, .3),
+    ##      // p_malicious (.15, .30, .45), p_txDistribution (.01, .05, .10),
+    ##      // and numRounds (10, 20). You should try to test your CompliantNode
+    ##      // code for all 3x3x3x2 = 54 combinations.
+    ##
     numNodes = 100
     p_graph = args[0] # // parameter for random graph: prob. that an edge will exist
     p_malicious = args[1] # // prob. that a node will be set to be malicious
@@ -26,19 +26,19 @@ def main(args):
     nodes = [None for i in range(numNodes)]
     for i in range(numNodes):
          if random.random() < p_malicious:
-            nodes[i] = MaliciousNode(p_graph, p_malicious, p_txDistribution, numRounds)
+             nodes[i] = MaliciousNode(p_graph, p_malicious, p_txDistribution, numRounds)
          else:
-            nodes[i] = CompliantNode(p_graph, p_malicious, p_txDistribution, numRounds)
+             nodes[i] = CompliantNode(p_graph, p_malicious, p_txDistribution, numRounds)
 
     followees = [[1 if random.random() < p_graph and i!= j else 0 for i in range(numNodes)] for j in range(numNodes)]
     for i in range(numNodes):
-         nodes[i].setFollowees(followees[i]);
+        nodes[i].setFollowees(followees[i]);
 
 ##      // initialize a set of 500 valid Transactions with random ids
     numTx = 500
     validTxIds = set()
     for i in range(numTx):
-         validTxIds.add(random.randint(1000,50000))
+        validTxIds.add(random.randint(1000,50000))
 
 
 ##      // distribute the 500 Transactions throughout the nodes, to initialize
@@ -49,47 +49,47 @@ def main(args):
     for i in range(numNodes):
         for txid in validTxIds:
           if (random.random() < p_txDistribution): #// p_txDistribution is .01, .05, or .10.
-                     pendingTransactions.add(Transaction(txid))
+              pendingTransactions.add(Transaction(txid))
 
         nodes[i].setPendingTransaction(pendingTransactions)
 
 
     for round in range(numRounds):
-##         // gather all the proposals into a map. The key is the index of the node receiving
-##         // proposals. The value is an List containing pairs. The first
-##         // element is the id of the transaction being proposed and the second
-##         // element is the index # of the node proposing the transaction.
+        ##         // gather all the proposals into a map. The key is the index of the node receiving
+        ##         // proposals. The value is an List containing pairs. The first
+        ##         // element is the id of the transaction being proposed and the second
+        ##         // element is the index # of the node proposing the transaction.
          allProposals = {}
 
          for i in range(numNodes):
-            proposals = nodes[i].sendToFollowers()
-            for tx in proposals:
-               if (tx not in validTxIds):
-                  break  #// ensure that each tx is actually valid
+             proposals = nodes[i].sendToFollowers()
+             for tx in proposals:
+                 if (tx not in validTxIds):
+                     break  #// ensure that each tx is actually valid
 
-               for j in range(numNodes):
-                   if (not followees[j][i]):
-                       break #continue; // tx only matters if j follows i
+                 for j in range(numNodes):
+                     if (not followees[j][i]):
+                         break #continue; // tx only matters if j follows i
 
-                   if (j not in allProposals):
-                       candidates = set()
-                       allProposals[j] = candidates
-                   candidate = Candidate(tx, i)
-                   allProposals[j].add(candidate) # I guess brackets here is what we should do?
+                     if (j not in allProposals):
+                         candidates = set()
+                         allProposals[j] = candidates
+                         candidate = Candidate(tx, i)
+                         allProposals[j].add(candidate) # I guess brackets here is what we should do?
 
 ##         // Distribute the Proposals to their intended recipients as Candidates
          for i in range(numNodes):
             if (i in allProposals):
-               nodes[i].receiveFromFollowees(allProposals.get(i));
+                nodes[i].receiveFromFollowees(allProposals.get(i));
 
 
 
  #     // print results
     for i in range(numNodes):
-         transactions = nodes[i].sendToFollowers()
-         print("Transaction ids that Node " + str(i) + " believes consensus on:")
-         for tx in transactions:
-             print(tx.id)
+        transactions = nodes[i].sendToFollowers()
+        print("Transaction ids that Node " + str(i) + " believes consensus on:")
+        for tx in transactions:
+            print(tx.id)
 
 # All of the arrays below are directly from the homework assignment
 # (except I added |30| to the final array, for fun)
